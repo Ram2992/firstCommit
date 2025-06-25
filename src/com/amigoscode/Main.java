ca462b364e075a8b33cdc53d57e2dff059f8130c
rambabu818 <ramtunduru818@gmail.com>
2025-06-25 08:20:31 +0530

    second commit

---
+++ src/com/amigoscode/Main.java
@@ -0,0 +1,10 @@
+
+package com.amigoscode;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to Amigoscode");
+        //to run java project in terminal
+        //Java packageName.className
+        //Java com.amigoscode.Main
+    }
+}
