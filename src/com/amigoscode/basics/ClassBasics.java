044814220b42387cc944cc67ed0d90b109d38c5b
rambabu818 <ramtunduru818@gmail.com>
2025-06-25 08:51:24 +0530

    4th commit

--- src/com/amigoscode/basics/ClassBasics.java
+++ src/com/amigoscode/basics/ClassBasics.java
@@ -1,4 +1,16 @@
 package com.amigoscode.basics;
 
 public class ClassBasics {
+
+    static ClassBasics cbObj = new ClassBasics();
+
+    public static void main(String[] args){
+        System.out.println("ClassBasics");
+        cbObj.bVariables();
+    }
+
+    public void bVariables(){
+
+
+    }
 }
